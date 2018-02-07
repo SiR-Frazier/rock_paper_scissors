73bedaa873f4835c738f119f5acb8a0cb8df62f9
Bryce Frazier and Maggie Summers <maggie@>
2018-02-07 10:10:44 -0800

    pass two tests

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,6 +4,10 @@ class RPS
   def wins?(player1, player2)
     if (player1 == "rock") && (player2 == "scissors")
       true
+    elsif (player1 == "scissors") && (player2 == "paper")
+      true
+    elsif (player1 == "paper") && (player2 == "rock")
+      true
     end
   end
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -6,4 +6,12 @@ describe('#rock_paper_scissors') do
     game = RPS.new()
     expect(game.wins?("rock", "scissors")).to(eq(true))
   end
+  it("returns true if scissors is the object and paper is the argument") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "paper")).to(eq(true))
+  end
+  it("returns true if paper is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins?("paper", "rock")).to(eq(true))
+  end
 end
