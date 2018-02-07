6be0c6a3e255061cac93921a59298aa52d089e2e
Bryce Frazier and Maggie Summers <maggie@>
2018-02-07 11:21:28 -0800

    edit code to be more efficient

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,25 +1,53 @@
-require('pry')
-
 class RPS
-  def wins?(player1, player2)
-    if (player1 == "rock") && (player2 == "scissors")
+  def initialize(choice)
+    @choice = choice
+  end
+
+  def choice
+    @choice
+  end
+
+  def wins?(opp_choice)
+    if (@choice == "rock" &&  opp_choice == "scissors") || (@choice == "scissors" && opp_choice == "paper") || (@choice == "paper" && opp_choice == "rock")
       true
-    elsif (player1 == "scissors") && (player2 == "paper")
-      true
-    elsif (player1 == "paper") && (player2 == "rock")
-      true
-    elsif (player1 == "scissors") && (player2 == "rock")
+    else
       false
-    elsif (player1 == "rock") && (player2 == "paper")
-      false
-    elsif (player1 == "paper") && (player2 == "scissors")
-      false
-    elsif (player1 == "paper") && (player2 == "paper")
-      "Play again"
-    elsif (player1 == "scissors") && (player2 == "scissors")
-    "Play again"
-    elsif (player1 == "rock") && (player2 == "rock")
-    "Play again"
     end
   end
 end
+
+
+# puts "Player 1: Rock, paper, scissors?"
+# outcome_player1 = gets.chomp
+# puts "Player 2: Rock, paper, scissors?"
+# outcome_player2 = gets.chomp
+#
+#
+#
+# if "#{outcome_player1}" ===
+#   puts "Player 1 Wins"
+#
+#
+#  "#{outcome_player2}")
+
+# def wins?(player1, player2)
+#   if (player1 == "rock") && (player2 == "scissors")
+#     true
+#   elsif (player1 == "scissors") && (player2 == "paper")
+#     true
+#   elsif (player1 == "paper") && (player2 == "rock")
+#     true
+#   elsif (player1 == "scissors") && (player2 == "rock")
+#     false
+#   elsif (player1 == "rock") && (player2 == "paper")
+#     false
+#   elsif (player1 == "paper") && (player2 == "scissors")
+#     false
+#   elsif (player1 == "paper") && (player2 == "paper")
+#     "Play again"
+#   elsif (player1 == "scissors") && (player2 == "scissors")
+#     "Play again"
+#   elsif (player1 == "rock") && (player2 == "rock")
+#     "Play again"
+#   end
+# end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -1,41 +1,66 @@
 require('rspec')
 require('rock_paper_scissors')
+require('pry')
 
 describe('#rock_paper_scissors') do
-  it("returns true if rock is the object and scissors is the argument") do
-    game = RPS.new()
-    expect(game.wins?("rock", "scissors")).to(eq(true))
+  it("returns true if player one wins") do
+    player1 = RPS.new("rock")
+    player2 = RPS.new("scissors")
+    expect(player1.wins?(player2.choice)).to(eq(true))
   end
-  it("returns true if scissors is the object and paper is the argument") do
-    game = RPS.new()
-    expect(game.wins?("scissors", "paper")).to(eq(true))
+  it("returns true if player one wins") do
+    player1 = RPS.new("scissors")
+    player2 = RPS.new("paper")
+    expect(player1.wins?(player2.choice)).to(eq(true))
   end
-  it("returns true if paper is the object and rock is the argument") do
-    game = RPS.new()
-    expect(game.wins?("paper", "rock")).to(eq(true))
+  it("returns true if player one wins") do
+    player1 = RPS.new("paper")
+    player2 = RPS.new("rock")
+    expect(player1.wins?(player2.choice)).to(eq(true))
   end
-  it("returns false if scissors is the object and rock is the argument") do
-    game = RPS.new()
-    expect(game.wins?("scissors", "rock")).to(eq(false))
-  end
-  it("returns false if rock is the object and paper is the argument") do
-    game = RPS.new()
-    expect(game.wins?("rock", "paper")).to(eq(false))
-  end
-  it("returns false if paper is the object and scissors is the argument") do
-    game = RPS.new()
-    expect(game.wins?("paper", "scissors")).to(eq(false))
-  end
-  it("returns 'Play again' if player1 and player2 have the same result") do
-    game = RPS.new()
-    expect(game.wins?("paper", "paper")).to(eq("Play again"))
-  end
-  it("returns 'Play again' if player1 and player2 have the same result") do
-    game = RPS.new()
-    expect(game.wins?("scissors", "scissors")).to(eq("Play again"))
-  end
-  it("returns 'Play again' if player1 and player2 have the same result") do
-    game = RPS.new()
-    expect(game.wins?("rock", "rock")).to(eq("Play again"))
+  it("returns false if player one loses") do
+    player1 = RPS.new("rock")
+    player2 = RPS.new("paper")
+    expect(player1.wins?(player2.choice)).to(eq(false))
   end
 end
+
+
+
+
+
+
+
+#   it("returns true if scissors is the object and paper is the argument") do
+#     game = RPS.new()
+#     expect(game.wins?("scissors", "paper")).to(eq(true))
+#   end
+#   it("returns true if paper is the object and rock is the argument") do
+#     game = RPS.new()
+#     expect(game.wins?("paper", "rock")).to(eq(true))
+#   end
+#   it("returns false if scissors is the object and rock is the argument") do
+#     game = RPS.new()
+#     expect(game.wins?("scissors", "rock")).to(eq(false))
+#   end
+#   it("returns false if rock is the object and paper is the argument") do
+#     game = RPS.new()
+#     expect(game.wins?("rock", "paper")).to(eq(false))
+#   end
+#   it("returns false if paper is the object and scissors is the argument") do
+#     game = RPS.new()
+#     expect(game.wins?("paper", "scissors")).to(eq(false))
+#   end
+#   it("returns 'Play again' if player1 and player2 have the same result") do
+#     game = RPS.new()
+#     expect(game.wins?("paper", "paper")).to(eq("Play again"))
+#   end
+#   it("returns 'Play again' if player1 and player2 have the same result") do
+#     game = RPS.new()
+#     expect(game.wins?("scissors", "scissors")).to(eq("Play again"))
+#   end
+#   it("returns 'Play again' if player1 and player2 have the same result") do
+#     game = RPS.new()
+#     expect(game.wins?("rock", "rock")).to(eq("Play again"))
+#   end
+# end
